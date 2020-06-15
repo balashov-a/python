e6e2cfe134ccd2770e1b9cef06f2040bfd370c8b
balashov-a <66442155+balashov-a@users.noreply.github.com>
2020-06-16 00:36:07 +0300

    Add files via upload

---
+++ L4_HW7.py
@@ -0,0 +1,9 @@
+def fact(number):
+    factorial = 1
+    for x in range(1, number + 1):
+        factorial *= x
+        yield factorial
+
+n = int(input("Введите число для которого будет расчитан факториал: "))
+for el in fact(n):
+    print(el)
